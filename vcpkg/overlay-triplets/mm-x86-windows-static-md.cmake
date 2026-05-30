c12df71bb26f870c96f6432eec527ffbd15a8df4
Aliaksei SyDr Karalenka <sydr1991@gmail.com>
2026-05-30 06:50:24 +0300

    Use platform 143 in vcpkg too

--- vcpkg/overlay-triplets/mm-x86-windows-static-md.cmake
+++ vcpkg/overlay-triplets/mm-x86-windows-static-md.cmake
@@ -3,7 +3,4 @@ set(VCPKG_DEP_INFO_OVERRIDE_VARS "v143")
 
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-set(VCPKG_CXX_FLAGS "/fsanitize=address")
-set(VCPKG_C_FLAGS "/fsanitize=address")
+set(VCPKG_LIBRARY_LINKAGE static)
